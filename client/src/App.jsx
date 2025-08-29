907b756f4290722baff040d38d8cd75dc81c221e
Shorya Rastogi <49874404+shorya2201@users.noreply.github.com>
2025-08-29 12:21:16 +0530

    Added layout to support navbar on every pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,7 +32,47 @@ const App = () => {
 
 
                                 {/* ADD THE ADMIN ROUTE */}
-                                <Route path="/admin" element={<AdminDashboard />} />
+                                <Route path="/import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Home from "./components/home/Home";
+import CampaignsDetils from "./components/campaign/CampaignsDetils";
+import CampaignPage from "./components/campaign/CampaignPage";
+import Startcampaign from "./components/campaign/Startcampaign";
+import Paynow from "./components/Paynow";
+import Layout from "./components/Layout"; // Import the new Layout component
+import AdminDashboard from "./components/admin/AdminDashboard";
+import AboutUs from "./components/AboutUs";
+import HowItWorks from "./components/HowItWorks";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* All routes are now nested inside the Layout route */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
+          <Route path="campaigns" element={<CampaignsDetils />} />
+          <Route path="campaigns/:id" element={<CampaignPage />} />
+          <Route path="start-campaign" element={<Startcampaign />} />
+          <Route path="campaigns/:id/donate" element={<Paynow />} />
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="aboutUs" element={<AboutUs />} />
+          <Route path="how-it-works" element={<HowItWorks />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
+admin" element={<AdminDashboard />} />
 
                                 <Route path="*" element={<div>404, Page Not Found</div>} />
                         </Routes>
