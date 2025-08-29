ffa3614a4d33a25682f8ce4239a4cb75745ac92c
Shorya Rastogi <49874404+shorya2201@users.noreply.github.com>
2025-08-29 12:25:35 +0530

    Solved deployment issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,41 +8,7 @@ import CampaignsDetils from "./components/campaign/CampaignsDetils";
 import CampaignPage from "./components/campaign/CampaignPage";
 import Startcampaign from "./components/campaign/Startcampaign";
 import Paynow from "./components/Paynow";
-
-import AboutUs from "./components/AboutUs";
-import HowItWorks from "./components/HowItWorks";
-import AdminDashboard from "./components/admin/AdminDashboard"; // Import the new component
-
-
-const App = () => {
-        return (
-                <Router>
-                        <Routes>
-                                <Route path="/" element={<Home />} />
-                                <Route path="/signup" element={<Signup />} />
-                                <Route path="/login" element={<Login />} />
-                                <Route path="/aboutUs" element={<AboutUs />} />
-                                <Route path="/how-it-works" element={<HowItWorks />} />
-                                <Route path="/aboutUs" element={<AboutUs />} /> {/* Add the new route */}
-                                <Route path="/how-it-works" element={<HowItWorks />} /> {/* Add the new route */}
-                                <Route path="/campaigns" element={<CampaignsDetils />} />
-                                <Route path="/campaigns/:id" element={<CampaignPage />} />
-                                <Route path="/start-campaign" element={<Startcampaign />} />
-                                <Route path="/campaigns/:id/donate" element={<Paynow />} />
-
-
-                                {/* ADD THE ADMIN ROUTE */}
-                                <Route path="/import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-import Home from "./components/home/Home";
-import CampaignsDetils from "./components/campaign/CampaignsDetils";
-import CampaignPage from "./components/campaign/CampaignPage";
-import Startcampaign from "./components/campaign/Startcampaign";
-import Paynow from "./components/Paynow";
-import Layout from "./components/Layout"; // Import the new Layout component
+import Layout from "./components/Layout";
 import AdminDashboard from "./components/admin/AdminDashboard";
 import AboutUs from "./components/AboutUs";
 import HowItWorks from "./components/HowItWorks";
@@ -51,9 +17,12 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        {/* All routes are now nested inside the Layout route */}
+        {/* All page routes are now nested inside the Layout route */}
         <Route path="/" element={<Layout />}>
+          {/* The index route displays the Home component at the root path "/" */}
           <Route index element={<Home />} />
+          
+          {/* Other page routes */}
           <Route path="signup" element={<Signup />} />
           <Route path="login" element={<Login />} />
           <Route path="campaigns" element={<CampaignsDetils />} />
@@ -63,7 +32,9 @@ const App = () => {
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="aboutUs" element={<AboutUs />} />
           <Route path="how-it-works" element={<HowItWorks />} />
-          <Route path="*" element={<div>404 Not Found</div>} />
+          
+          {/* Catch-all route for 404 Not Found pages */}
+          <Route path="*" element={<div className="text-center p-10">404 Not Found</div>} />
         </Route>
       </Routes>
     </Router>
@@ -72,12 +43,3 @@ const App = () => {
 
 export default App;
 
-admin" element={<AdminDashboard />} />
-
-                                <Route path="*" element={<div>404, Page Not Found</div>} />
-                        </Routes>
-                </Router>
-        );
-};
-
-export default App;
