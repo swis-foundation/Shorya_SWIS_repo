b8d4709e9ab05d21b872b1e6b4343e53725a4412
Shorya Rastogi <49874404+shorya2201@users.noreply.github.com>
2025-09-06 15:31:55 +0530

    Added new color palettes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-background': '#FFFBF5', // Warm Creamy White
+        'brand-primary': '#9C3353',    // Milaap's Pink/Maroon
+        'brand-primary-hover': '#872b47',
+        'brand-secondary': '#F5D0A9',  // Light Accent
+        'brand-text': '#333333',      // Dark text
+        'brand-text-light': '#555555', // Lighter text
+      }
+    },
   },
   plugins: [],
 }
